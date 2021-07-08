48645b0b8157e29f4b6c1e2a2ff0aefc21a835fc
emir.vranac <vranacemir@gmail.com>
2021-07-08 10:19:17 +0200

    Fixes in the text content

--- components/layout.js
+++ components/layout.js
@@ -4,8 +4,8 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = 'Example Title'
-export const siteTitle = 'Next.js Sample Website'
+const name = 'Next.js Blog'
+export const siteTitle = 'Next.js Blog'
 
 export default function Layout({children, home}) {
    return  <div className={styles.container}>
