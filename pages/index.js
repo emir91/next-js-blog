f256eb6c1528c0dac7fcfa72bb286e5e86b23b03
emir.vranac <vranacemir@gmail.com>
2021-07-08 10:17:47 +0200

    Fixes in the text content

--- pages/index.js
+++ pages/index.js
@@ -21,10 +21,10 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Short description about this post</p>
+        <p>Welcome to Next.js Blog</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          (This is a sample website done as a part of{' '}
+          <a href="https://nextjs.org/learn">Next.js tutorial</a>.)
         </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
